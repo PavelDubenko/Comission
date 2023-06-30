244d76d75b489ffcc0aea9d548ae4f85431187f1
Pavel Dubenko <pavel.dubenko@fstravel.com>
2023-06-30 17:59:05 +0300

    initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,33 @@
 fun main () {
+     println(calculateCommission("VK Pay", 5000, 1000))
+}
 
+fun calculateCommission(cardType: String, previousTransfers: Int, transferAmount: Int): Double {
+    val mastercardMaestroLimit = 75_000
+    val visaMirMinimum = 35
+    val mastercardMaestroCommissionRate = 0.006
+    val mastercardMaestroCommissionFixed = 20
+    val visaMirCommissionRate = 0.0075
+
+
+    var commission = 0.0
+
+    when (cardType) {
+        "Mastercard", "Maestro" -> {
+            if (previousTransfers >= mastercardMaestroLimit) {
+                commission = transferAmount * mastercardMaestroCommissionRate + mastercardMaestroCommissionFixed
+            }
+        }
+        "Visa", "Мир" -> {
+            commission = transferAmount * visaMirCommissionRate
+            if (commission < visaMirMinimum) {
+                commission = visaMirMinimum.toDouble()
+            }
+        }
+        "VK Pay" -> {
+            commission = 0.0
+        }
+    }
+
+    return commission
 }
